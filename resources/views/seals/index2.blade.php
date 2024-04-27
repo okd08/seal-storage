71185acb43e30b0dfc4345fbac11d818b75268d1
okd08 <149041290+okd08@users.noreply.github.com>
2024-04-27 17:21:50 +0900

    微修正

--- resources/views/seals/index2.blade.php
+++ resources/views/seals/index2.blade.php
@@ -10,7 +10,6 @@
     <p class="text-bold text-lg mb-6 font-bold text-center">┈୨୧┈ パッケージ管理 ┈୨୧┈</p>
     @foreach ($packages as $p)
       <div class="flex bg-white rounded-lg border-2 border-yellow-200 text-lg font-bold py-2 px-3 mb-3">
-        <p class="mr-5">{{ $p->id . '.' }}</p>
         <p class="mr-2">{{ $p->name }}</p>
         <a href="{{ route('packages.edit', $p) }}" class="ml-auto bg-sky-300 hover:bg-sky-400 px-3 text-white rounded">編集</a>
       </div>
